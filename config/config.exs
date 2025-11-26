5fd535d927860eec6c9134247d6a3e04af80b2fc
julianlinaress <julian@linares.com.ar>
2025-11-26 19:38:24 -0300

    mix phx.gen.auth Accounts User users

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :service_hub, :scopes,
+  user: [
+    default: true,
+    module: ServiceHub.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: ServiceHub.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :service_hub,
   ecto_repos: [ServiceHub.Repo],
   generators: [timestamp_type: :utc_datetime]
